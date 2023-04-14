94a81610ec89c1b9468c7f2b7a1c55838fe23303
madlfie8017 <madlife8017@naver.com>
2023-04-14 17:11:04 +0900

    교육용

--- 06-1_집합연산자.sql
+++ 06-1_집합연산자.sql
@@ -1,7 +1,17 @@
---집합연산자
---UNION(합집합 중복x, UNION ALL(합집합 중복 O),
---INTERSECT(교집합), MINUS(차집합)(차집합은 순서 중요)
---위 아래 conlumn 개수와 데이터 타입이 정확히 일치해야 합니다.
+
+-- 집합 연산자
+-- UNION(합집합 중복x), UNION ALL(합집합 중복 o), INTERSECT(교집합), MINUS(차집합)
+-- 위 아래 column 개수와 데이터 타입이 정확히 일치해야 합니다.
+
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+UNION
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
 
 SELECT
     employee_id, first_name
@@ -13,7 +23,6 @@ SELECT
 FROM employees
 WHERE department_id = 20;
 
-
 SELECT
     employee_id, first_name
 FROM employees
@@ -24,6 +33,15 @@ SELECT
 FROM employees
 WHERE department_id = 20;
 
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE hire_date LIKE '04%'
+MINUS
+SELECT
+    employee_id, first_name
+FROM employees
+WHERE department_id = 20;
 
 SELECT
     employee_id, first_name
@@ -34,3 +52,11 @@ SELECT
     employee_id, first_name
 FROM employees
 WHERE hire_date LIKE '04%';
+
+
+
+
+
+
+
+
